d6e294559ea0c1541774c458ea55532c6b97cc76
dpuentex <dpuente1097@gmail.com>
2023-12-03 22:30:53 -0800

    finished callback

--- move.js
+++ move.js
@@ -1,12 +1,57 @@
 function move(element) {
-    element.style.position = 'fixed'
+  element.style.position = "fixed";
 
-    function moveToCoordinates(left, bottom) {
-        element.style.left = left + 'px'
-        element.style.bottom = bottom + 'px'
-    }
+  function moveToCoordinates(left, bottom) {
+    element.style.left = left + "px";
+    element.style.bottom = bottom + "px";
+  }
 
-    return {
-        to: moveToCoordinates
-    }
+  function moveWithArrowKeys(left, bottom, callback) {
+    let direction = null;
+    let x = left;
+    let y = bottom;
+
+    document.addEventListener("keydown", function (e) {
+      if (e.key === "ArrowLeft") {
+        direction = "west";
+      }
+      if (e.key === "ArrowUp") {
+        direction = "north";
+      }
+      if (e.key === "ArrowRight") {
+        direction = "east";
+      }
+      if (e.key === "ArrowDown") {
+        direction = "south";
+      }
+      callback(direction);
+    });
+
+    setInterval(function () {
+      if (direction === "west") {
+        x = x - 1;
+      }
+      if (direction === "north") {
+        y = y + 1;
+      }
+      if (direction === "east") {
+        x = x + 1;
+      }
+      if (direction === "south") {
+        y = y - 1;
+      }
+      character.style.left = x + "px";
+      character.style.bottom = y + "px";
+    }, 1);
+
+    document.addEventListener("keyup", function (e) {
+      direction = null;
+      callback(direction);
+    });
+  }
+
+  return {
+    to: moveToCoordinates,
+    withArrowKeys: moveWithArrowKeys,
+  };
 }
